d28b72ff0f880f92124be8015313657808a869e8
John Steel <jsteel-cc@conestogac.on.ca>
2015-03-14 18:26:11 -0400

    Moved content into a content div, added global js file that enables jquery mobile domCache. Demonstration on gear.html

---
+++ js/global.js
@@ -0,0 +1,8 @@
+/**
+ * Created by John on 2015-03-14.
+ */
+$(function(){
+    // Disable reloading of page content so that form
+    // data will persist.
+    $.mobile.page.prototype.options.domCache = true;
+});

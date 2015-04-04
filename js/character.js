d6380f0cb0e9a3e46c25a2955fabb768ed3432e4
John Steel <jsteel-cc@conestogac.on.ca>
2015-04-04 12:12:34 -0400

    Character now must be loaded last because it relies on data from most other classes (Table names). Character's build tables function will now build all relational tables. addSpell and removeSpell methods update char object & websql.

--- js/character.js
+++ js/character.js
@@ -1,4 +1,4 @@
-/*globals $, Database*/
+/*globals $, Database, Feat, Spell, Hazard*/
 /**
  * Character Class
  * @param name The name of the character
@@ -8,7 +8,9 @@ function Character(name) {
     "use strict";
     this.name = name || "Unnamed Character";
     this.feats = [];
+    this.hazard = [];
     this.spells = [];
+    this.skills = [];
     this.bab = 0;
     this.level = 0;
 }
@@ -16,13 +18,73 @@ function Character(name) {
 Character.TABLE_NAME = "Characters"
 
 /**
- * Save to websql
+ * Add a spell to the Character
+ * @param {(Number|String)} id - The spell id
+ * @param {Function} [success] - Executed after success
  */
-Character.prototype.save = function () {
+Character.prototype.addSpell = function (id, success) {
     "use strict";
-    console.log("Saving \"" + this.name + "\"");
-    // TODO: FILL PERSIST CHAR CODE
-    console.log("\"" + this.name + "\" saved");
+    if (!this.id) {
+        console.error(this.name + " does not have a valid ID");
+    }
+    var afterSql;
+    var characterId = this.id;
+    var itemTableName = Spell.TABLE_NAME;
+    var joinTableName = Character.TABLE_NAME + itemTableName;
+    var sql = "INSERT INTO " + joinTableName + "(" +
+        Character.TABLE_NAME + "Id, " + itemTableName + "Id) VALUES (?, ?)";
+
+    // If spell not already added, add it to object & persist to websql
+    if (this.spells.indexOf(id) === -1) {
+        this.spells.push(id); // TODO: Only add to object on successful SQL insert
+        Database.transaction(function (tx) {
+            tx.executeSql(sql, [characterId, id], afterSql, afterSql);
+        });
+    }
+
+    afterSql = function (tx, response) {
+        if (/SQLError/.test(response)) {
+            throw response.message;
+        }
+
+        if (success) {
+            success();
+        }
+    };
+};
+
+/**
+ * Remove a spell from the Character
+ * @param {(Number|String)} id - The spell id
+ * @param {Function} [success] - Executed after success
+ */
+Character.prototype.removeSpell = function (id, success) {
+    "use strict";
+    var afterSql;
+    var characterId = this.id;
+    var itemTableName = Spell.TABLE_NAME;
+    var joinTableName = Character.TABLE_NAME + itemTableName;
+    var spellArrayIndex = this.spells.indexOf(id);
+    var sql = "DELETE FROM " + joinTableName + " WHERE " +
+        Character.TABLE_NAME + "Id = ? AND " + itemTableName + "Id = ?";
+
+    // If spell added, remove it from object & websql
+    if (spellArrayIndex !== -1) {
+        this.spells.splice(spellArrayIndex, 1); // TODO: Only remove successful SQL delete
+        Database.transaction(function (tx) {
+            tx.executeSql(sql, [characterId, id], afterSql, afterSql);
+        });
+    }
+
+    afterSql = function (tx, response) {
+        if (/SQLError/.test(response)) {
+            throw response.message;
+        }
+
+        if (success) {
+            success();
+        }
+    };
 };
 
 /**
@@ -33,7 +95,7 @@ Character.prototype.save = function () {
 Character.createTable = function (success, rebuild) {
     "use strict";
     var createTableFunction, afterSql;
-    var sql = 'CREATE TABLE ' + Character.TABLE_NAME +
+    var createCharTableSql = 'CREATE TABLE ' + Character.TABLE_NAME +
         '(' +
         '  id INTEGER PRIMARY KEY,' +
         '  name varchar(50),' +
@@ -42,8 +104,14 @@ Character.createTable = function (success, rebuild) {
         '  level TINYINT' +
         ')';
 
+    var joinTables = [Feat.TABLE_NAME, Spell.TABLE_NAME, Hazard.TABLE_NAME, "Skills"];
+    var createJoinTableSql = 'CREATE TABLE ? ( ' +
+        '?Id INTEGER, ' +
+        '?Id INTEGER' +
+        ')';
+
     createTableFunction = function (tx) {
-        tx.executeSql(sql, [], afterSql, afterSql);
+        tx.executeSql(createCharTableSql, [], afterSql, afterSql);
     };
 
     afterSql = function (tx, response) {
@@ -59,6 +127,13 @@ Character.createTable = function (success, rebuild) {
             } else {
                 console.error(response.message);
             }
+        } else if (joinTables.length > 0) {
+            var joinTable = joinTables.pop();
+            tx.executeSql(createJoinTableSql
+                .replace("?", Character.TABLE_NAME + joinTable)
+                .replace("?", Character.TABLE_NAME)
+                .replace("?", joinTable),
+                [], afterSql, afterSql);
         } else if (success) {
             success();
         }

7c73e83b652d677db717958d87a34d1355f30fc4
John Steel <jsteel-cc@conestogac.on.ca>
2015-04-07 14:58:14 -0400

    Setup test folder

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,14 +1,14 @@
 module.exports = function(config){
   config.set({
 
-    basePath : './',
+    basePath : '../',
 
     files : [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
     ],
 
     autoWatch : true,
@@ -20,8 +20,7 @@ module.exports = function(config){
     plugins : [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
+            'karma-jasmine'
             ],
 
     junitReporter : {
